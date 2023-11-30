6baca0c7bf8f85e00a31a7cb4966438837aa2034
Imran Hussain <imranhussain399@gmail.com>
2023-11-30 14:21:13 +0500

    Add Header navigation links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,25 @@ import { Link } from "react-router-dom";
 import cart from "../../assets/cart-1.svg";
 
 const Header = ({ totalItems }) => {
-  return;
+  return (
+    <header className="header-section">
+      <h1>Howdi Waodi</h1>
+      <nav>
+        <Link to={"/products"} className="nav-link products">
+          Products
+        </Link>
+        <Link to={"/home"} className="nav-link home">
+          Home
+        </Link>
+        <Link to={"/shop-cart"} className="nav-link shop-cart">
+          <img className="cart-icon" src={cart} alt="shop cart" />
+          {Object.keys(totalItems).length > 0 && (
+            <p className="total-items">{Object.keys(totalItems).length} </p>
+          )}
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export { Header };
