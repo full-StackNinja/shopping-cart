7e8214052e9e24ced3b8d05bbf87e4622e24c92a
Imran Hussain <imranhussain399@gmail.com>
2023-11-30 14:03:56 +0500

    Add react router and add default route to the router

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// Import default stylesheets
+import "./defaultStyles/meyer-reset-1st.css";
+import "./defaultStyles/my-css-reset-2nd.css";
+import "./defaultStyles/normalize-3rd.css";
+import "./defaultStyles/typography-4th.css";
+import "./components/App/App.css";
+
+import { App } from "./components/App/App";
+import { MainContent } from "./components/MainContent";
+import { ErrorPage } from "./components/ErrorPage";
+import { ProductDetailPage } from "./components/ProductDetailPage";
+import { Home } from "./components/Home";
+import { ShopCart } from "./components/ShopCart";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
