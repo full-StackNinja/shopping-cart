d95d3a6cb79550d470bb7bfd56ddb387593ef75d
Imran Hussain <imranhussain399@gmail.com>
2023-12-01 21:51:05 +0500

    Define ShopCart component to list down all products in the cart

--- src/components/ShopCart.jsx
+++ src/components/ShopCart.jsx
@@ -1,3 +1,39 @@
+import { useOutletContext } from "react-router-dom";
+import { CartProduct } from "./CartProduct";
+
 export const ShopCart = () => {
-  return
+  const [totalItems, setTotalItems] = useOutletContext();
+  const totalItemsList = Object.values(totalItems);
+
+  const handleIncrement = (productId, quantity) => {
+    const modifiedItems = { ...totalItems };
+    const reqProduct = modifiedItems[productId];
+    reqProduct.quantity = quantity < 9 ? quantity + 1 : 9;
+    setTotalItems(modifiedItems);
+  };
+
+  const handleDecrement = (productId, quantity) => {
+    const modifiedItems = { ...totalItems };
+    const reqProduct = modifiedItems[productId];
+    reqProduct.quantity = quantity > 1 ? quantity - 1 : 1;
+    setTotalItems(modifiedItems);
+  };
+  return (
+    <div className="cart-list">
+      <ul>
+        {totalItemsList.map((item) => {
+          console.log("item keys are", item.product.id);
+          return (
+            <li key={item.product.id}>
+              <CartProduct
+                item={item}
+                handleDecrement={handleDecrement}
+                handleIncrement={handleIncrement}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
