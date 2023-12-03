0609f96df7d02d661e60ba481168cb305547496b
Imran Hussain <imranhussain399@gmail.com>
2023-12-03 21:14:37 +0500

    Add 'Add to Cart' button for each product

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom";
 
-function ProductCard({ product }) {
+function ProductCard({ product, handleAddToCart }) {
   return (
     <div className="product">
-      <Link
-        to={`/product-detail-page/${product.id}`}
-        className="product-link"
-        state={{ product }}
-      >
+      <Link to={`/product-detail-page/${product.id}`} className="product-link" state={{ product }}>
         <img src={product.image} alt={product.title} />
         <div className="short-details">
           <p className="product-title">{product.title}</p>
           <p className="product-price">{product.price + "$"}</p>
         </div>
       </Link>
+      <button
+        className="add-to-cart"
+        onClick={() => {
+          handleAddToCart(product);
+        }}
+      >
+        Add to Cart
+      </button>
     </div>
   );
 }
