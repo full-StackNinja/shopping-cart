5e292674e4092c183e48564c6795aa5f96e0c9ec
Imran Hussain <imranhussain399@gmail.com>
2023-12-10 22:06:08 +0500

    Typo correction

--- src/components/ThankYouMessage.jsx
+++ src/components/ThankYouMessage.jsx
@@ -4,7 +4,7 @@ export const ThankYouMessage = () => {
   return (
     <div className="order-placement-page">
       <h1 className="order-placed-text">Order placed successfully!</h1>
-      <h2 className="thanks-msg">Thak You for shopping with us.</h2>
+      <h2 className="thanks-msg">Thank You for shopping with us.</h2>
       <Link to={"/products"}>
         <button className="shop-more">Shop More</button>
       </Link>
