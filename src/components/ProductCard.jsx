d43b7809c5e7f8b06c5e8e1bf1c0bf33a4ff2257
Imran Hussain <imranhussain399@gmail.com>
2023-12-10 22:04:59 +0500

    Line break change due to vs editor setting change

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,8 +2,12 @@ import { Link } from "react-router-dom";
 
 function ProductCard({ product, handleAddToCart }) {
   return (
-    <div className="product">
-      <Link to={`/product-detail-page/${product.id}`} className="product-link" state={{ product }}>
+    <div data-testid="product" className="product">
+      <Link
+        to={`/product-detail-page/${product.id}`}
+        className="product-link"
+        state={{ product }}
+      >
         <img src={product.image} alt={product.title} />
         <div className="short-details">
           <p className="product-title">{product.title}</p>
