e1f6c39ee612408af800a94930e1c214677cdb83
Imran Hussain <imranhussain399@gmail.com>
2023-12-10 21:53:55 +0500

    Correct typo error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ export const Home = () => {
     <div className="home-section">
       <h1 className="brand-name">Howdi Waodi</h1>
       <p className="brand-info">A name to be trusted</p>
-      <h1>Shop from the veriety of the items!</h1>
+      <h1>Shop from the variety of the items!</h1>
     </div>
   );
 };
